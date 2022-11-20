4a7223944e60b34857936730a9fbd6b2741e9d53
Ardhan12 <arief.ramadhan43@gmail.com>
2022-11-20 22:04:36 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const MyHomePage(title: 'Arip gajadi'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Arip Apps"),
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.mail),
+            ),
+          ],
+        ),
+        body: SafeArea(
+          child: Text("Aku cina kamu"),
+        ),
+      ),
+    );
+  }
+}
